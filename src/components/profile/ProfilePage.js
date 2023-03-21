8530507bc5d03ba515bd73e8743c5ecda4cb4f19
OrubieraR <107748119+OrubieraR@users.noreply.github.com>
2023-03-21 04:04:41 +0100

    User adverts in user profile page.

--- src/components/profile/ProfilePage.js
+++ src/components/profile/ProfilePage.js
@@ -1,50 +1,128 @@
-import { useTranslation } from "react-i18next";
-import {  useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
-import "../../style/profile.css";
-import DeleteAccount from "./DeleteAccount";
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import i18n from '../../i18n';
+import { useTranslation } from 'react-i18next';
+// import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { getUserAdvert } from '../../store/slices/adverts';
+import '../../style/profile.css';
+import DeleteAccount from './DeleteAccount';
+import { Link } from 'react-router-dom';
 //import FavoritesPage from "./Favorites";
 
-const ProfilePage = () => {
-  const { userInfo } = useSelector((state) => state.user);
-  const [t] = useTranslation("translation");
+const EmptyList = () => {
+	return (
+		<div>
+			<p>{i18n.t('No products')}</p>
+		</div>
+	);
+};
 
-  return (
-    <section className="profile-page">
-      <div>
-        <nav className="nav-profile">
-          <NavLink className="nav-user" to="/user-profile/my-favorites">
-            | {t("Favourites")} |
-          </NavLink>
-          <NavLink className="nav-user">| {t("Reserved")} |</NavLink>
-        </nav>
-      </div>
-      <div className="profile-data">
-        <section>
-          <figure src={"img/uwu-profile.png"} alt="avatar">
-            {userInfo?.name}
-          </figure>
-          <ul>
-            <li>
-              {t("Name")} : {userInfo?.name}
-            </li>
-            <li>
-              {t("Email")} : {userInfo?.email}
-            </li>
-          </ul>
-          <div>
-            <NavLink> {t("Edit profile")}</NavLink>
-          </div>
-          <div>
-            <DeleteAccount />
-          </div>
-        </section>
-        <section className="my-adverts">
-          <h1>{t("My Adverts")}</h1>
-        </section>
-      </div>
-    </section>
-  );
+const ProfilePage = () => {
+	const { userInfo } = useSelector((state) => state.user);
+	const [t] = useTranslation('translation');
+	const userOwner = userInfo?.name;
+	const dispatch = useDispatch();
+	const url = process.env.REACT_APP_URL_PHOTO;
+
+	//Traer los anuncios
+	useEffect(() => {
+		dispatch(getUserAdvert(userOwner));
+	}, [dispatch, userOwner]);
+
+	const userAdverts = useSelector((state) => state.adverts.list);
+
+	const [visible, setVisible] = useState(2);
+	const [isCompleted, setIsCompleted] = useState(false);
+	const allGames = userAdverts.length;
+
+	const showMoreGames = () => {
+		setVisible((prevValue) => {
+			const nextGames = prevValue + 6;
+			if (nextGames > allGames) {
+				setIsCompleted(true);
+				return allGames;
+			}
+			return nextGames;
+		});
+	};
+
+	return (
+		<section className='profile-page'>
+			<div>
+				<nav className='nav-profile'>
+					<NavLink className='nav-user' to='/user-profile/my-favorites'>
+						| {t('Favourites')} |
+					</NavLink>
+					<NavLink className='nav-user'>| {t('Reserved')} |</NavLink>
+				</nav>
+			</div>
+			<div className='profile-data'>
+				<section>
+					<figure src={'img/uwu-profile.png'} alt='avatar'>
+						{userInfo?.name}
+					</figure>
+					<ul>
+						<li>
+							{t('Name')} : {userInfo?.name}
+						</li>
+						<li>
+							{t('Email')} : {userInfo?.email}
+						</li>
+					</ul>
+					<div>
+						<NavLink> {t('Edit profile')}</NavLink>
+					</div>
+					<div>
+						<DeleteAccount />
+					</div>
+				</section>
+				<section className='my-adverts'>
+					<h1>{t('My Adverts')}</h1>
+					<div className='advertsPage'>
+						{allGames > 0 ? (
+							<ul className='advertsPage-list'>
+								{userAdverts?.slice(0, visible).map((item) => (
+									<li className='advertsPage-item' key={item._id}>
+										<Link className='linkDetail' to={`/${item._id}`}>
+											<div className='AdvertDetail-photo'>
+												{item.photo ? (
+													<img src={url + item.photo} alt={item.photo} />
+												) : (
+													<img
+														src={'img/image-coming-soon.jpg'}
+														alt='coming-soon'
+													/>
+												)}
+											</div>
+											<p>{item.name}</p>
+											<p>
+												{t('Price')}: {item.price}$
+											</p>
+											<p>
+												{t('State')}: {item.sale ? t('On sale') : t('Buying')}
+											</p>
+											<p>
+												{' '}
+												{t('Category')}: {item.category.toString()}{' '}
+											</p>
+										</Link>
+									</li>
+								))}
+							</ul>
+						) : (
+							<EmptyList />
+						)}
+					</div>
+					<div>
+						<button onClick={showMoreGames} disabled={isCompleted}>
+							Load More {visible}/{allGames}
+						</button>
+					</div>
+				</section>
+			</div>
+		</section>
+	);
 };
 
 export default ProfilePage;
--- src/store/slices/adverts.js
+++ src/store/slices/adverts.js
@@ -1,55 +1,65 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
-import client from "../../api/client";
+import client from '../../api/client';
 
-const advertUrl = "/api/adverts";
+const advertUrl = '/api/adverts';
 
 export const advertsSlice = createSlice({
-  name: "adverts",
-  initialState: {
-    list: [],
-  },
-  reducers: {
-    setAdvertsList: (state, action) => {
-      state.list = action.payload;
-    },
-    setAdvertDetail: (state, action) => {
-      state.list = [action.payload];
-    },
-    setDeletedAdvert: (state, action) => {
-      state.list = state.list.filter((advert) => advert.id !== action.payload);
-    },
-  },
+	name: 'adverts',
+	initialState: {
+		list: [],
+	},
+	reducers: {
+		setAdvertsList: (state, action) => {
+			state.list = action.payload;
+		},
+		setAdvertDetail: (state, action) => {
+			state.list = [action.payload];
+		},
+		setDeletedAdvert: (state, action) => {
+			state.list = state.list.filter((advert) => advert.id !== action.payload);
+		},
+	},
 });
 
 export const fetchAllAdverts = () => (dispatch) => {
-  client
-    .get(advertUrl)
-    .then((response) => {
-      dispatch(setAdvertsList(response.results.reverse()));
-    })
-    .catch((error) => console.log(error));
+	client
+		.get(advertUrl)
+		.then((response) => {
+			dispatch(setAdvertsList(response.results.reverse()));
+		})
+		.catch((error) => console.log(error));
 };
 
 export const getUniqueAdvert = (advertId) => (dispatch) => {
-  client
-    .get(`${advertUrl}/${advertId}`)
-    .then((response) => {
-      dispatch(setAdvertDetail(response.results));
-    })
-    .catch((error) => console.log(error));
+	client
+		.get(`${advertUrl}/${advertId}`)
+		.then((response) => {
+			dispatch(setAdvertDetail(response.results));
+		})
+		.catch((error) => console.log(error));
 };
 
 export const deleteAdvert = (advert) => (dispatch) => {
-  const advertId = advert._id;
-  client
-    .delete(`${advertUrl}/${advertId}`)
-    .then((response) => {
-      dispatch(setDeletedAdvert(response.results));
-    })
-    .catch((error) => console.log(error));
+	const advertId = advert._id;
+	client
+		.delete(`${advertUrl}/${advertId}`)
+		.then((response) => {
+			dispatch(setDeletedAdvert(response.results));
+		})
+		.catch((error) => console.log(error));
 };
+
+export const getUserAdvert = (userOwner) => (dispatch) => {
+	client
+		.get(`${advertUrl}/user/${userOwner}`)
+		.then((response) => {
+			dispatch(setAdvertsList(response.results.reverse()));
+		})
+		.catch((error) => console.log(error));
+};
+
 export const { setAdvertsList, setAdvertDetail, setDeletedAdvert } =
-  advertsSlice.actions;
+	advertsSlice.actions;
 
 export default advertsSlice.reducer;
